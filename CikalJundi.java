ded3ab33538e122ac5b27eee256a993ed6aa5730
Tianditho <146790148+Tianditho@users.noreply.github.com>
2025-12-21 16:39:12 +0700

    Update CikalJundi.java

--- CikalJundi.java
+++ CikalJundi.java
@@ -1,6 +1,6 @@
 public class CikalJundi {
 
     public static void main(String[] args) {
-        System.out.println("Saya CikalJundi - NIM:1324065");
+        System.out.println("Saya Cikal Jundi Farabi - NIM:1324065");
     }
 }
